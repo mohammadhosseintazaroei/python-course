c4ff8b644b7be9552ec4e1751a0bf944b515c635
mohammad753 <mh@local.net>
2023-05-26 06:32:53 -0700

    twentyFirstDay

---
+++ computationalOperatorsInClassesTwentyFirstDay.py
@@ -0,0 +1,15 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+
+point = Point(3, 5)
+another = Point(1, 9)
+print(point + another)
